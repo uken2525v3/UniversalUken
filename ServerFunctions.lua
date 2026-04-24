d98558a728d22cb4fd6e35e7da4f315c6862e5d6
uken2525v3 <uken2525v3@gmail.com>
2026-04-25 02:03:27 +0700

    Create ServerFunctions.lua

---
+++ ServerFunctions.lua
@@ -0,0 +1,36 @@
+if not game:IsLoaded() then
+	game.Loaded:Wait()
+end
+
+local server = {}
+local totalServer = {}
+
+local Players = game:GetService("Players")
+local HttpService = game:GetService("HttpService")
+local TeleportService = game:GetService("TeleportService")
+
+local JobId = game.JobId
+local PlaceId = game.PlaceId
+
+local LocalPlayer = Players.LocalPlayer
+
+function server.hop()
+	local serverList = game:HttpGet("https://games.roblox.com/v1/games/" .. game.PlaceId .. "/servers/Public?sortOrder=Desc&limit=100&excludeFullGames=true")
+	local serverListTable = serverList and HttpService:JSONDecode(serverList)
+	
+	if serverListTable then
+		totalServer = {}
+		for _, serverData in ipairs(serverListTable) do
+			table.insert(totalServer, serverData.id)
+		end
+	end
+	
+	local selectJobId = totalServer[math.random(1, #totalServer)]
+	TeleportService:TeleportToPlaceInstance(PlaceId, selectJobId, LocalPlayer)
+end
+
+function server.rejoin()
+	TeleportService:TeleportToPlaceInstance(PlaceId, JobId, LocalPlayer)
+end
+
+return server
